29ceb31306218f6f64ac1e9cfe3719621ef04931
Rei <rii1101e@gmail.com>
2026-02-20 23:58:17 +0300

    Edited 2193C

--- p800-1000/ReplaceandSum2193C.cpp
+++ p800-1000/ReplaceandSum2193C.cpp
@@ -2,32 +2,38 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int t;
     cin >> t;
     while (t--) {
         int n, q;
         cin >> n >> q;
+        vector<long long> a(n + 2), b(n + 2);
 
-        vector<long long> a(n+1), b(n+1);
-        for (int i = 1; i <= n; i++) cin >> a[i];
-        for (int i = 1; i <= n; i++) cin >> b[i];
-
-        vector<long long> suf(n+2, 0);
-        for (int i = n; i >= 1; i--)
-            suf[i] = max(a[i], suf[i+1]);
-
-        vector<long long> pre(n+1, 0);
         for (int i = 1; i <= n; i++)
-            pre[i] = pre[i-1] + max(suf[i], b[i]);
+            cin >> a[i];
+        for (int i = 1; i <= n; i++)
+            cin >> b[i];
+
+        vector<long long> best(n + 2, 0);
+        for (int i = n; i >= 1; i--) {
+            best[i] = max({a[i], b[i], best[i + 1]});
+        }
+
+        vector<long long> pref(n + 2, 0);
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i - 1] + best[i];
+        }
 
         while (q--) {
             int l, r;
             cin >> l >> r;
-            cout << pre[r] - pre[l-1] << " \n"[q == 0];
+            long long ans = pref[r] - pref[l - 1];
+            cout << ans << " ";
         }
+        cout << "\n";
     }
     return 0;
 }
