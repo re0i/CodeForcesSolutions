990579e1e189287d89a74d7e78a2457df67159be
Rei <rii1101e@gmail.com>
2026-02-27 23:28:47 +0300

    Added EatinGameA

---
+++ Round 1084 Div 3/EatingGameA.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        int mx = 0;
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            mx = max(mx, a[i]);
+        }
+
+        int countMax = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == mx)
+                countMax++;
+        }
+
+        cout << countMax << "\n";
+    }
+
+    return 0;
+}
